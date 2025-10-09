fcf259e9bd633c3069ee9001fe17feb1b3b94124
Adarshr55 <adarshdarshan861@gmail.com>
2025-10-09 12:25:57 +0530

    Time: 2 ms (29.24%) | Memory: 56 MB (96.89%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,6 +3,11 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    return nums.concat(nums)
+    let arr=[]
+    for(i=0;i<nums.length;i++){
+        arr.push(nums[i])
+    }
+        return nums.concat(arr)
+    
     
 };
