431837b55d1b3bc0011e0cd0e2e2c4ce84993bf4
Adarshr55 <adarshdarshan861@gmail.com>
2025-11-05 20:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (5.68%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,7 +3,17 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-    let output=s.toLowerCase()
-    return output
+    let result =""
+    for(char of s){
+        let code=char.charCodeAt(0)
+        if(code>=65  && code<=90){
+            result+=String.fromCharCode(code+32)
+        }else{
+            result+=char
+
+        }
+        
+    }
+    return result
 
 };
