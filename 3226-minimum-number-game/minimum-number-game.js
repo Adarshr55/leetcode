9e7451d13129be9b7c667b9a48ecc2eefa4844a4
Adarshr55 <adarshdarshan861@gmail.com>
2025-11-05 20:05:39 +0530

    Time: 3 ms (55.53%) | Memory: 56.4 MB (87.72%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    let res=[]
+    nums.sort((a,b)=>a-b)
+   for (let i=0;i<nums.length; i+=2){
+    let alice=nums[i]
+    let bob=nums[i+1]
+    res.push(bob,alice)
+   }
+   return res
+
+
+};
