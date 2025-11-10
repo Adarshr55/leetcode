462dd66db29c647ef0aa70713cc82d8a620b3587
Adarshr55 <adarshdarshan861@gmail.com>
2025-11-10 11:44:26 +0530

    Time: 2 ms (35.66%) | Memory: 57.7 MB (24.81%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} moves
+ * @return {number}
+ */
+var furthestDistanceFromOrigin = function(moves) {
+    let L=0
+    let R=0
+    let U=0
+    for(let i=0;i<moves.length;i++){
+        if(moves[i]==="L"){
+            L++
+        }   
+        else if(moves[i]==="R"){
+            R++
+        }
+          else U++
+    }
+    return Math.abs(L-R )+U
+};
