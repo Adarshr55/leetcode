a325b5a2b7936644c4d11e5cc4af4a8e2e30ab4d
Adarshr55 <adarshdarshan861@gmail.com>
2025-11-20 15:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (71.70%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -4,13 +4,7 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    let s1=""
-    let s2=""
-    for( str of word1){
-        s1+=str
-    }
-    for(str of word2){
-        s2+=str
-    }
-    return s1===s2
+   let s1=word1.join("")
+   let s2=word2.join("")
+   return s1===s2
 };
