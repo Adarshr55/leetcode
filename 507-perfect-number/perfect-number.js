834864ad55590902e2eeccc3e980b83cf2a1c0b3
Adarshr55 <adarshdarshan861@gmail.com>
2025-11-27 21:07:30 +0530

    Time: 2122 ms (5.39%) | Memory: 53.7 MB (84.48%) - LeetSync

--- 507-perfect-number/perfect-number.js
+++ 507-perfect-number/perfect-number.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-
-    if(num<1)return false
-    let sum=0
-    for(i=1;i<=num/2;i++){
-        if(num%i==0){
-            sum+=i
-        }
+ if(num<0)return false
+let sum=0
+for(i=1;i<num;i++){
+    if(num%i==0){
+        sum+=i
     }
-    return sum==num
-
+}
+    if(num===sum){
+        return true
+    }else
+    return false
     
 };
