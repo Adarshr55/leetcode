1006f89c08b6ee022ba3111dc10e6869dbff0dfe
Adarshr55 <adarshdarshan861@gmail.com>
2026-01-07 20:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.13%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        s= bin(n)
-        count=0
-        for i in s:
-            if i=="1":
-                count+=1
-        return count
+        return bin(n).count("1")
