8d6efff7e93e49b40a08f309214288b6821c031c
Adarsh R <adarshdarshan861@gmail.com>
2026-01-29 22:31:06 +0530

    Time: 234 ms (21.04%) | Memory: 12.5 MB (73.99%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,20 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        max_val=max(nums)
-        count=[0]*(max_val+1)
-        for num in nums:
-            count[num]+=1
-
-        for i in range(1,len(count)):
-
-            count[i]+=count[i-1]
-
-        result=[]
-        for num in nums:
-            if num==0:
-                result.append(0)
-            else:
-                result.append(count[num-1])
-        return result
+        n=len(nums)
+        arr=[]
+        for i in range(n):
+            count=0
+            for j  in range(n):
+                if nums[i]>nums[j]:
+                    count+=1
+            arr.append(count)
+        return arr
         
+            
+
+
