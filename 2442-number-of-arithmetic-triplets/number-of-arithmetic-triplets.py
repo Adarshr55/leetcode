5a14123270a291470c2f9b68d7fc961211cdd742
Adarsh R <adarshdarshan861@gmail.com>
2026-02-26 10:10:36 +0530

    Time: 7 ms (49.83%) | Memory: 12.5 MB (29.97%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -5,9 +5,9 @@ class Solution(object):
         :type diff: int
         :rtype: int
         """
-        newset=set(nums)
+        # newset=set(nums)
         count=0
         for i in range(len(nums)):
-            if nums[i]+diff in newset and nums[i]+ 2*diff in newset:
+            if nums[i]+diff in nums and nums[i]+ 2*diff in nums:
                 count+=1
         return count
