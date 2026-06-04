73a234dea222ac360c5b0e96d8d422a9bdad8aa9
Adarsh R <adarshdarshan861@gmail.com>
2026-06-04 18:09:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.61%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -4,4 +4,6 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        return num ==0 or num % 10 !=0
+        s=int(str(num)[::-1])
+        s1=int(str(s)[::-1])
+        return num==s1
